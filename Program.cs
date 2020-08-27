d6811fc4c8a40226bb4dd1451eecc29a0b028dbe
11ange <luis.lange@gmail.com>
2020-08-27 11:07:23 -0300

    Exercício 2 finalizado

--- Program.cs
+++ Program.cs
@@ -2,48 +2,67 @@
 
 namespace GFT
 {
-    /*3) Considerando os conceitos de Orientação a Objetos, crie uma classe abstrata Pai de
-    nome Conta com os seguintes atributos (número, titular e saldo) e mais duas classes Filhas
-    (ContaCorrente e ContaPoupanca). Na classe Conta deve existir um método de nome
-    rendimento que retorna o rendimento mensal da conta, nas classes filhas deve existir o
-    mesmo método rendimento, porém com as seguintes regras:
-    Para ContaCorrente, o método rendimento deve retornar o saldo * 0.03;
-    Para ContaPoupanca, o método rendimento deve retornar o saldo * 0.05;
-    Por fim, criar uma classe principal que instancie objetos de ContaCorrente e
-    ContaPoupanca, adicione no mínimo um valor para cada atributo e imprima cada conta
-    (ContaCorrente e ContaPoupanca) com seus devidos rendimentos.
-    Obs: Encapsular atributos das classes.*/
+    /*2) Crie uma Classe Pessoa, contendo os atributos (nome, endereço e telefone)
+encapsulados, com seus respectivos seletores (getters) e modificadores (setters), e ainda o
+construtor padrão. Por fim, criar uma classe principal que instancie o objeto Pessoa,
+adicione no mínimo um valor para cada atributo e imprima na tela.*/
     class Program
     {
         static void Main(string[] args)
         {
-            ContaCorrente cc = new ContaCorrente();
-            ContaPoupanca cp = new ContaPoupanca();
-            cc.
+            Pessoa Joaquim = new Pessoa();
+            Joaquim.Endereco = "Rua quatro, 55";
+            Joaquim.Nome = "Joaquim da Silva";
+            Joaquim.Telefone = "(11) 9999-9999";
+            Console.WriteLine();
+            Console.WriteLine(Joaquim.Endereco);
+            Console.WriteLine(Joaquim.Nome);
+            Console.WriteLine(Joaquim.Telefone);
+            Console.WriteLine();
         }
     }
-    public class Conta
+    public class Pessoa
     {
-        protected int numero { get; set; }
-        private string titular { get; set; }
-        protected double saldo { get; set; }
-        public void rendimento()
+        private string nome;
+        public string Nome
         {
-            
+            get
+            {
+                return this.nome;
+            }
+            set
+            {
+                this.nome = value;
+            }
         }
-    }
-    public class ContaCorrente : Conta
-    {
-        public double rendimento()
+        private string endereco;
+                public string Endereco
         {
-            return saldo * 0.03;
+            get
+            {
+                return this.endereco;
+            }
+            set
+            {
+                this.endereco = value;
+            }
         }
-    }
-    public class ContaPoupanca : Conta
-    {
-        public double rendimento()
+        private string telefone;
+                public string Telefone
         {
-            return saldo * 0.05;
+            get
+            {
+                return this.telefone;
+            }
+            set
+            {
+                this.telefone = value;
+            }
         }
+        public Pessoa()
+        {
+
+        }
+
     }
 }
